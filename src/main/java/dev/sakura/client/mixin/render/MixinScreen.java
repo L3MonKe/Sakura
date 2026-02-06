10c9cb24899d44c7cbc75bedafc2bd25051d5c51
MZC8865 <yolandahe@163.com>
2026-02-06 20:54:40 +0200

    Revert "优化mainscreem"
    This reverts commit 1fd7828dd51b4f8355e3cbd2abbe6db34fb9b8e9.

--- src/main/java/dev/sakura/client/mixin/render/MixinScreen.java
+++ src/main/java/dev/sakura/client/mixin/render/MixinScreen.java
@@ -37,10 +37,14 @@ public class MixinScreen {
     @Inject(method = "renderPanoramaBackground", at = @At("HEAD"), cancellable = true)
     public void renderPanoramaBackgroundHook(DrawContext context, float delta, CallbackInfo ci) {
         if (mc.world == null) {
-            MainMenuShader.getSharedInstance().render(this.width, this.height);
+            if (mainMenuShader == null) {
+                mainMenuShader = new MainMenuShader(MainMenuShader.MainMenuShaderType.CUTE);
+            }
+            mainMenuShader.render(this.width, this.height);
             ci.cancel();
-        } else {
-            MainMenuShader.cleanupSharedInstance();
+        } else if (mainMenuShader != null) {
+            mainMenuShader.cleanup();
+            mainMenuShader = null;
         }
     }
 }
