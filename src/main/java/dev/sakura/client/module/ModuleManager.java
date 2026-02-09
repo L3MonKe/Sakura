785a2bdff048de09efcacb95f6e380f880edf805
L3MonKe <hhcrystal2008@gmail.com>
2026-02-10 01:02:31 +0800

    [-] AutoThrow

--- src/main/java/dev/sakura/client/module/ModuleManager.java
+++ src/main/java/dev/sakura/client/module/ModuleManager.java
@@ -38,7 +38,6 @@ public class ModuleManager {
         add(new AutoCrystal());
         add(new AutoAnchor());
         add(new AutoTotem());
-        add(new AutoThrow());
         add(new LegitCrystal());
         add(new SafeHotbar());
 
